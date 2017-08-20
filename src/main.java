feab3c1f644b900b222d59056f29bfcca21761f8
Bobandirus <sb823249@reading.ac.uk>
2017-08-20 11:18:12 +0100

    v7 19/03/15. Attempt to isolate family, genus, and species descriptions, and contain their descriptions with them. Also modifying the matcher to include whether what you are currently maching is an epithet or a description, so it can place the name in the database instead of a boolean

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
 /**
- * Created by sb823249 on 12/03/2015.
+ * Created by sb823249 on 19/03/2015.
  */
 
 import java.io.*;
@@ -16,167 +16,180 @@ public class main {
     static String[] tableHeadings = new String[0];
     static String fileName = "";
     static String lineToAdd = "";
+    static boolean familyCount = true;
+    static boolean genusCount = true;
+    static boolean speciesCount = true;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+
         int fileNumber = 0;
-        //for (int c = 1; c < 9; c++){
-        for (int c = 7; c < 8; c++){
+        for (int c = 1; c < 40; c++) {
             fileNumber = c;
-            //fileName = "C:\\Users\\sb823249\\Google Drive\\PhD!\\OCR thoughts\\scans\\pinaceae\\ocring\\" + fileNumber + ".txt";
-            fileName = "" + System.getProperty("user.dir") + "\\" + fileNumber + ".txt";
+            //fileName = "" + System.getProperty("user.dir") + "\\text post adaptations\\" + fileNumber + ".txt";
+            fileName = "" + System.getProperty("user.dir") + "\\stace text mk1.txt";
             readDescription();
         }
         writeFile();
         fileName = "";
+
+        //firstStep();
     }
 
-    public static void readDescription() throws IOException{
-        //List<String> descriptiveInput = Files.readAllLines(Paths.get("C:\\Users\\sb823249\\Google Drive\\PhD!\\OCR thoughts\\ocrd text to database\\test one\\1.txt"), StandardCharsets.UTF_8);
-        List<String> descriptiveInput = Files.readAllLines(Paths.get(fileName), StandardCharsets.ISO_8859_1);
-        for (int c = 0; c < descriptiveInput.size(); c++){
-            if (descriptiveInput.size() > 1){
-                System.out.print("your file did a bad");
-            }
+    /*public static void firstStep() throws IOException{
+        int fileNumber = 0;
+        for (int c = 1; c < 40; c++) {
+            fileNumber = c;
+            //fileName = "" + System.getProperty("user.dir") + "\\text post adaptations\\" + fileNumber + ".txt";
+            fileName = "" + System.getProperty("user.dir") + "\\stace text mk1.txt";
+            //readDescription();
+        }
+        writeFile();
+        fileName = "";
+    }*/
+
+    public static void readDescription() throws IOException {
+        //List<String> descriptiveInput = Files.readAllLines(Paths.get(fileName), StandardCharsets.ISO_8859_1);
+        List<String> descriptiveInput = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        String[] input = new String[descriptiveInput.size()];
+        input = descriptiveInput.toArray(input);
+        for (int c = 0; c < input.length; c++) {
             String[] preBitsOne = descriptiveInput.get(c).split(";");
             ArrayList<String> bitsAdder = new ArrayList<String>();
-            for (int b = 0; b < preBitsOne.length; b++){
+            for (int b = 0; b < preBitsOne.length; b++) {
+                Pattern familyName = Pattern.compile("\\. \\.*ACEAE");
+                Pattern genusName = Pattern.compile("\\. [A-Z][A-Z][A-Z][A-Z][A-Z]");
                 Pattern speciesName = Pattern.compile("\\. [A-Z]\\. [a-z]");
-                Pattern endOfLine = Pattern.compile("\\. [A-Z]\\. ");
+                Matcher familyPattern = familyName.matcher(preBitsOne[b]);
+                Matcher genusPattern = genusName.matcher(preBitsOne[b]);
                 Matcher speciesPattern = speciesName.matcher(preBitsOne[b]);
-                Matcher lineEnder = endOfLine.matcher(preBitsOne[b]);
-                if (speciesPattern.find()){
+                if (familyPattern.find()) {
                     bitsAdder.add(preBitsOne[b]);
-                }
-                else if (lineEnder.find()){
-                    String[] preAndPost = bits[b].split("\\. [1-99]\\. ");
-                    bitsAdder.add(preAndPost[0]);
-                    bitsAdder.add("\n");
-                    bitsAdder.add(preAndPost[1]);
-                }
-                else {
+                } else if (genusPattern.find()) {
+                    bitsAdder.add(preBitsOne[b]);
+                } else if (speciesPattern.find()) {
+                    bitsAdder.add(preBitsOne[b]);
+                } else {
                     String[] addToAdder = preBitsOne[b].split("\\.");
-                    for (int r = 0; r < addToAdder.length; r++){
+                    for (int r = 0; r < addToAdder.length; r++) {
                         String addingToAdder = addToAdder[r];
-                        //System.out.print(addingToAdder);
                         bitsAdder.add(addingToAdder);
                     }
                 }
             }
             bits = bitsAdder.toArray(bits);
-            for (int q = 0; q < bits.length; q++){
-                System.out.print(bits[q] + "\n");
+            for (int q = 0; q < bits.length; q++) {
+                //System.out.print(bits[q] + "\n");
             }
-            //System.out.print(Arrays.toString(bits) + "\n");
         }
         readHeadings();
     }
 
-    public static void readHeadings() throws IOException{
-        //List<String> tableReader = Files.readAllLines(Paths.get("C:\\Users\\sb823249\\Google Drive\\PhD!\\OCR thoughts\\scans\\pinaceae\\ocring\\database.csv"), StandardCharsets.UTF_8);
+    public static void readHeadings() throws IOException {
         List<String> tableReader = Files.readAllLines(Paths.get("" + System.getProperty("user.dir") + "\\database.csv"), StandardCharsets.UTF_8);
-        for (int c = 0; c < tableReader.size(); c++){
+        for (int c = 0; c < tableReader.size(); c++) {
             tableHeadings = tableReader.get(c).split(",");
         }
         compareEntries();
     }
 
     public static void compareEntries() throws IOException {
-        Boolean firstTime = true;
-        int get = 0;
-        int sizeOne = Array.getLength(tableHeadings);
-        int sizeTwo = Array.getLength(bits);
-        int sizeThree = 0;
+        Boolean speciesFirst = true;
+        Boolean genusFirst = true;
+        Boolean familyFirst = true;
         String adder = "";
         ArrayList<String> buildingLine = new ArrayList<String>();
         String[] line = new String[tableHeadings.length];
-        if (sizeOne < sizeTwo) {
-            sizeThree = sizeTwo;
-        } else if (sizeOne >= sizeTwo) {
-            sizeThree = sizeOne;
-        }
-        for (int c = 0; c < tableHeadings.length; c++){
-            for (int b = 0; b < sizeThree; b++){
-                Pattern speciesName = Pattern.compile("\\. [A-Z]\\. [a-z]");
-                Pattern endOfLine = Pattern.compile("\\. \\. ");
-                Matcher speciesPattern = speciesName.matcher(bits[b]);
-                Matcher lineEnder = endOfLine.matcher(bits[b]);
-                if (firstTime && speciesPattern.find()){
+        for (int c = 0; c < tableHeadings.length; c++) {
+            boolean familyHeading = false;
+            boolean genusHeading = false;
+            boolean speciesHeading = false;
+            String family = "Family";
+            String genus = "Genus";
+            String species = "Name";
+            if (tableHeadings[c].equals(family)) {
+                familyHeading = true;
+            }
+            if (tableHeadings[c].equals(genus)) {
+                genusHeading = true;
+            }
+            if (tableHeadings[c].equals(species)) {
+                speciesHeading = true;
+            }
+            bitsChecker:
+            for (int b = 0; b < bits.length; b++) {
+                bits[b] = bits[b].replace(",", "");
+                Boolean familyFound = false;
+                Boolean genusFound = false;
+                Boolean speciesFound = false;
+                String[] bitsOne = bits[b].split(" - ");
+                nameFinder:
+                for (int n = 0; n < bitsOne.length; n++) {
+                    String part = bitsOne[n];
+                    Pattern genuSs = Pattern.compile("[A-Z][A-Z][A-Z]");
+                    Matcher genusMatcher = genuSs.matcher(part);
+                    Pattern specieSs = Pattern.compile("\\. [A-Z]\\. [a-z]");
+                    Matcher speciesMatcher = specieSs.matcher(part);
+                    if (part.endsWith("ACEAE")) {
+                        familyFound = true;
+                        break nameFinder;
+                    } else if (genusMatcher.find()) {
+                        genusFound = true;
+                        break nameFinder;
+                    } else if (speciesMatcher.find()) {
+                        speciesFound = true;
+                        break nameFinder;
+                    }
+                }
+                if ((familyFound) && familyHeading) {
                     line[c] = bits[b];
-                    firstTime = false;
-                }
-                else if (lineEnder.find()){
-                    line[c] = "\n";
-                }
-                else if (bits[b].contains(" ") && tableHeadings[c].contains(" ")){
+                    speciesFirst = false;
+                    break bitsChecker;
+                } else if (genusFound && genusHeading) {
+                    line[c] = bits[b];
+                    genusFirst = false;
+                    genusFound = false;
+                    break bitsChecker;
+                } else if (speciesFound && speciesHeading) {
+                    line[c] = bits[b];
+                    speciesFirst = false;
+                    speciesFound = false;
+                    break bitsChecker;
+                } else if (bits[b].contains(" ") && tableHeadings[c].contains(" ")) {
                     boolean added = false;
-                    //add replace charictor with blank here to get rid of punctuation which'll confuse the reader
-                    //It all needs to be gone if its joined onto a letter. EG "linear," or "trimporphic- "
+                    bits[b] = bits[b].toLowerCase();
+                    tableHeadings[c] = tableHeadings[c].toLowerCase();
                     bits[b] = bits[b].replace("- ", " ");
                     bits[b] = bits[b].replace(",", "");
                     String[] bitsBits = bits[b].split(" ");
                     String[] headingsBits = tableHeadings[c].split(" ");
-                    int stepperOne = Array.getLength(bitsBits);
-                    int stepperTwo = Array.getLength(headingsBits);
-                    int stepperThree = 0;
-                    String addMore = "";
-                    if (stepperOne > stepperTwo) {
-                        stepperThree = stepperOne;
-                    }
-                    if (stepperOne < stepperTwo) {
-                        stepperThree = stepperTwo;
-                    }
-                    if (Arrays.asList(bitsBits).containsAll(Arrays.asList(headingsBits))){
+                    if (Arrays.asList(bitsBits).containsAll(Arrays.asList(headingsBits))) {
                         line[c] = "yes";
                         added = true;
+                        break bitsChecker;
+                    } else {
+                        line[c] = "no";
+                        System.out.print("Column missing for " + bits[b] + "\n");
                     }
-                    else {
-                        for (int a = 0; a < headingsBits.length; a++){
-                            if (Arrays.asList(bitsBits).contains(headingsBits[a])){
-                                System.out.print("collumn missing for " + bits[b] + "\n");
-                                line[c] = "no";
-                            }
-                        }
-                    }
-                    if (added = false){
-                        if (line[c] != "yes"){
+                    if (added = false) {
+                        if (line[c] != "yes") {
                             line[c] = "no";
                         }
                     }
-                }
-                else if (bits[b].contains(tableHeadings[c])){
+                } else if (bits[b].contains(tableHeadings[c])) {
                     line[c] = "yes";
-                }
-
-                else {
-                    //if (line[c] != "yes"){
-                    if (line[c] == null){
+                    break bitsChecker;
+                } else {
+                    if (line[c] == null) {
+                        System.out.print("collumn missing for " + bits[b] + "\n");
                         line[c] = "no";
                     }
                 }
             }
         }
-        /*
-        System.out.print(Arrays.toString(line));
-        String headders = Arrays.toString(tableHeadings);
-        String headint = headders.replace("[", "");
-        String headetete = headint.replace(", ", ",");
-        String headed = headetete.replace("]", "");
-        FileWriter writer = new FileWriter("C:\\Users\\sb823249\\Google Drive\\PhD!\\OCR thoughts\\ocrd text to database\\test one\\output.csv");
-        writer.append(headed + "\n");
-        String data = Arrays.toString(line);
-        String dataA = data.replace("[", "");
-        String dataB = dataA.replace("]", "");
-        writer.append(dataB + "\n");
-        writer.close();
-        System.out.print("\n" + Arrays.toString(tableHeadings) + "\n");
-        System.out.print(Arrays.toString(line));
-        */
-        firstTime = true;
         lineAdder(line);
-        //argTest(line);
     }
 
-    public static void lineAdder(String[] line) throws IOException{
+    public static void lineAdder(String[] line) throws IOException {
         String data = Arrays.toString(line);
         String dataA = data.replace("[", "");
         String dataB = dataA.replace("]", "");
@@ -184,8 +197,7 @@ public class main {
         bits = new String[0];
     }
 
-    public static void writeFile() throws IOException{
-        //FileWriter writer = new FileWriter("C:\\Users\\sb823249\\Google Drive\\PhD!\\OCR thoughts\\scans\\pinaceae\\ocring\\output.csv");
+    public static void writeFile() throws IOException {
         FileWriter writer = new FileWriter("" + System.getProperty("user.dir") + "\\output.csv");
         String headders = Arrays.toString(tableHeadings);
         String headint = headders.replace("[", "");
@@ -195,13 +207,4 @@ public class main {
         writer.append(lineToAdd + "\n");
         writer.close();
     }
-
-    /*
-    public static void argTest(String [] line) throws IOException{
-        System.out.print(Arrays.toString(line));
-    }
-    */
-
-
 }
-
