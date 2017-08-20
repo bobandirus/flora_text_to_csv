7a8580129c3ec1a61920875ca9bb975ad0940a18
Bobandirus <sb823249@reading.ac.uk>
2017-08-20 10:49:16 +0100

    v3 06/03/15. New description reader with inital attempts to try and catch a species epithet. Moved the database builder to a more useful area

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
 /**
- * Created by sb823249 on 05/03/2015.
+ * Created by sb823249 on 06/03/2015.
  */
 
 import java.io.*;
@@ -8,10 +8,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class main {
     static String[] bits = new String[0];
     static String[] tableHeadings = new String[0];
+    static String[] addLine = new String[0];
 
     public static void main(String[] args) throws IOException {
         readDescription();
@@ -22,6 +25,8 @@ public class main {
     //if the element of the arraylist is "Capital letter, full stop, space, lowercase letter", then it keeps that
     //seperate because thats a species name (name bit not implimented yet)
     //
+
+    /*
     public static void readDescription() throws IOException {
         // makes a list array from all lines of the specified file
         List<String> descriptiveInput = Files.readAllLines(Paths.get("C:\\Users\\sb823249\\Google Drive\\PhD!\\OCR thoughts\\ocrd text to database\\test one\\test.txt"), StandardCharsets.UTF_8);
@@ -45,6 +50,60 @@ public class main {
         System.out.print("\n  \n");
         readHeadings();
     }
+    */
+
+    //
+    //This is a new version of readDescription() to try and get the species name to be treated seperately.
+    //I think there is an issue owth the regex in the first if statement in the second for loop, which is where
+    //the species name would be split off.
+    //this method may well be the way to get this thing to work, I just can't think my way through the logic at
+    //3pm on a friday afternoon
+    //
+    // Later note - maybe try a catch part?
+    //
+    public static void readDescription() throws IOException {
+        List<String> descriptiveInput = Files.readAllLines(Paths.get("C:\\Users\\sb823249\\Google Drive\\PhD!\\OCR thoughts\\ocrd text to database\\test one\\1.txt"), StandardCharsets.UTF_8);
+        for (int c = 0; c < descriptiveInput.size(); c++) {
+            if (descriptiveInput.size() > 1) {
+                System.out.print("your input file did a bad");
+            }
+            String[] preBitsOne = descriptiveInput.get(c).split(";");
+            ArrayList<String> bitsAdder = new ArrayList<String>();
+            for (int b = 0; b < preBitsOne.length; b++) {
+                //List<String> bitsAdder = new List<String>();
+                //System.out.print("\nhello" + preBitsOne[b]);
+                //if (preBitsOne[b].contains("[A-Z]\\. .")){
+                //if (preBitsOne[b].contains("\\p{javaUpperCase}\\. .")){
+                //if (preBitsOne[b].contains("False")){
+                //if (preBitsOne[b].contains("&")){
+                //if (preBitsOne[b].contains([a-z])){
+
+                // .contains doesnt work with regex!
+
+                Pattern p = Pattern.compile("[A-Z]\\. [a-z]");
+                Matcher m = p.matcher(preBitsOne[b]);
+                /*
+                if (m.find()) {
+                    //if (preBitsOne[b].matches("[A-Z]\\. [a-z]")){
+                    //bitsAdder.add(preBitsOne[b]);
+                    System.out.print("\n\n\n\n\nfinally!");
+                } else {
+                */
+                String[] addToAdder = preBitsOne[b].split("\\.");
+                for (int r = 0; r < addToAdder.length; r++) {
+                    String addingToAdder = addToAdder[r];
+                    bitsAdder.add(addingToAdder);
+                    // }
+                }
+            }
+            bits = bitsAdder.toArray(bits);
+            for (int q = 0; q < bits.length; q++) {
+                System.out.print(bits[q] + "\n");
+            }
+        }
+        readHeadings();
+    }
+
 
     //reads the database headings file and puts them in a seperate array. other than simply splitting on , only,
     //it uses the same method as before
@@ -80,8 +139,19 @@ public class main {
         for (int c = 0; c < tableHeadings.length; c++) {
             for (int b = 0; b < sizeThree; b++) {
                 int noColumn = 0;
-                if (bits[b].contains("^[A-Z]+$" + "\\. .")){
+                //this is trying to catch the spcies name, with the aim to put it under in the column with the
+                //heading 'name', but the regex may be wrong at the moment. or possibly the logic is wrong
+                // further up the program
+                /*
+                if (bits[b].contains("^[A-Z]+$\\. .")){
                     System.out.print("name is " + bits[b] + "!");
+                }
+                */
+                Pattern p = Pattern.compile("[A-Z]\\. [a-z]");
+                Matcher m = p.matcher(bits[b]);
+                if (m.find()){
+                    line[c] = bits[b];
+                    System.out.print("\n\nyay");
                 }
                 else if (bits[b].contains(" ") && tableHeadings[c].contains(" ")) {
                     boolean added = false;
@@ -145,6 +215,7 @@ public class main {
             }
 
 
+            /*
             String headders = Arrays.toString(tableHeadings);
             String headint = headders.replace("[", "");
             String headetete = headint.replace(", ", ",");
@@ -156,8 +227,20 @@ public class main {
             String dataB = dataA.replace("]", "");
             writer.append(dataB + "\n");
             writer.close();
+            */
         }
-
+        //addLine]
+        String headders = Arrays.toString(tableHeadings);
+        String headint = headders.replace("[", "");
+        String headetete = headint.replace(", ", ",");
+        String headed = headetete.replace("]", "");
+        FileWriter writer = new FileWriter("C:\\Users\\sb823249\\Google Drive\\PhD!\\OCR thoughts\\ocrd text to database\\test one\\output.csv");
+        writer.append(headed + "\n");
+        String data = Arrays.toString(line);
+        String dataA = data.replace("[", "");
+        String dataB = dataA.replace("]", "");
+        writer.append(dataB + "\n");
+        writer.close();
         System.out.print(Arrays.toString(line));
     }
 }
